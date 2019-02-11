893fcb8632867f6d2087e4381507f7a388f0feec
julianrobotham <julian.robotham@digital.homeoffice.gov.uk>
2019-02-11 11:28:17 +0000

    MSOE-1 task: inital commit
    Includes exported list of  organsiations that report modern slavery

---
+++ README.md
@@ -0,0 +1,20 @@
+# Modern Slavery Organisations
+
+Exports a list of organisations that report modern slavery.
+
+Can be used with the [Typeahead Aria](https://github.com/UKHomeOffice/typeahed-aria), which is exported with [HOF Frontend Assets](https://github.com/UKHomeOfficeForms/hof-frontend-assets).
+
+## Example Usage
+
+The following is an example of Modern Slavery Organisations in a HOF `field` with Typeahead Aria:
+```
+'country-select'-step: {
+  mixin: 'select',
+  className: ['typeahead', 'js-hidden'],
+  options: [''].concat(require('ms-organisations')),
+  legend: {
+    className: 'visuallyhidden'
+  },
+  validate: ['required']
+},
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const organisations = [
+    {value: 'Barnardos', label: 'Barnardos'},
+    {value: 'BAWSO', label: 'BAWSO'},
+    {value: 'Border Force', label: 'Border Force'},
+    {value: 'Gangmasters and Labour Abuse Authority', label: 'Gangmasters and Labour Abuse Authority'},
+    {value: 'Health and social care trust', label: 'Health and social care trust'},
+    {value: 'Home Office Immigration Enforcement', label: 'Home Office Immigration Enforcement'},
+    {value: 'Home Office Visas and Immigration', label: 'Home Office Visas and Immigration'},
+    {value: 'Kalayaan', label: 'Kalayaan'},
+    {value: 'Local authority', label: 'Local authority'},
+    {value: 'Medaille Trust', label: 'Medaille Trust'},
+    {value: 'Migrant Help', label: 'Migrant Help'},
+    {value: 'New Pathways', label: 'New Pathways'},
+    {value: 'NSPCC', label: 'NSPCC'},
+    {value: 'Police', label: 'Police'},
+    {value: 'Poppy Project', label: 'Poppy Project'},
+    {value: 'Refugee Council', label: 'Refugee Council'},
+    {value: 'Salvation Army', label: 'Salvation Army'},
+    {value: 'Tara Project', label: 'Tara Project'},
+    {value: 'Local authority', label: 'Local authority'},
+    {value: 'Unseen', label: 'Unseen'},
+  ];
+
+  module.exports = organisations;
